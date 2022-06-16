98865015990553283039066224a889d9ff375b83
FROIDEFOND Olivier <o.froidefond@exploitation.lomaco.fr>
2022-06-16 16:57:52 +0200

    correction

--- src/main/java/com/safety/safetyNet/service/SafetyNetService.java
+++ src/main/java/com/safety/safetyNet/service/SafetyNetService.java
@@ -11,9 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
-
+/**
+ * @author o.froidefond
+ */
 @Slf4j
 @Service
 public class SafetyNetService {
@@ -21,12 +25,19 @@ public class SafetyNetService {
     @Autowired
     SafetyNetRepository safetyNetRepository;
 
+    /**
+     * Fonction pour récupérer tout les mails d'une ville.
+     * @param city le nom de la ville
+     * @return Une liste de mail trié
+     */
     public List<Email> getAllMail(String city){
 
      ListSafety data = safetyNetRepository.getData();
      ArrayList<Persons> dataPersons = data.getPersons();
 
-        List<Email> listEmail = new ArrayList<>();
+        ArrayList<Email> listEmail = new ArrayList<>();
+
+
 
         //todo garder que les mail unique
         if (data != null) {
@@ -35,16 +46,15 @@ public class SafetyNetService {
                 if (city.equals(dataMail.getCity())){
                     Email mail = new Email();
                     mail.setEmail(dataMail.getEmail());
+
                     listEmail.add(mail);
                 }
             }
+
         } else {
             return null;
         }
+
       return listEmail;
     }
-
-
-
-
 }
